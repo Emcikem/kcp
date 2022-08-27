8fc50e6991e3fb35987eed75902750b5f00ea26f
Emcikem <18968479985@163.com>
2022-08-27 21:50:02 +0800

    init

---
+++ kcp-base/src/test/java/com/emcikem/kcpbase/KcpBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emcikem.kcpbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcpBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
